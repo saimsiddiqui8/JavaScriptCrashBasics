a23254d6cc5d3bc0b8dd705af86f03190c5e3377
Muhammad Saim Ali Siddiqui <106109352+saimsiddiqui8@users.noreply.github.com>
2023-04-16 14:21:41 +0500

    basic-1
    Basics class One

---
+++ basic-1/app.js
@@ -0,0 +1,70 @@
+
+
+
+//Write a Js function that joins two names.
+
+// function names(name1, name2) {
+//     // #proProgrammer
+//     console.log(name1 + " " + name2);
+// }
+// names("sehar", "fatima")
+
+// Write a function called isEven that takes a number
+// as an argument and returns true if the number
+// is even, and false otherwise.
+
+// function isEven(number) {
+//     // #proProgrammer2
+//     if (number % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(isEven(5));
+
+
+
+// Write a function that takes a number as
+//  an argument and returns that number squared.
+
+// function square(number) {
+//     // #proProgrammer3
+//     return number * number;
+// }
+// console.log(square(5));
+
+// Write a function that takes two numbers as arguments
+//  and returns the larger of the two.
+
+// function whichIsGreater(num1, num2) {
+//     // #proProgrammer4 **
+//     if (num1 > num2) {
+//         return num1;
+//     } else {
+//         return num2;
+//     }
+// }
+// console.log(whichIsGreater(50, 68));
+
+
+// Write a function that takes an array of numbers
+//  as an argument and returns the sum of all the
+//   numbers in the array.
+
+
+// function sum(array) {
+//     // #proProgrammer5 ***
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         sum += array[i];
+//     }
+//     return sum;
+// }
+// let arr = [18, 82, 32, 84, 54];
+// console.log(sum(arr));
+
+
+// Write a function that takes two numbers 
+// as arguments and returns their product.
+
