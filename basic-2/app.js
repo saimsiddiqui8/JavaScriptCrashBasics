bf64f36d3431eec07339761042b885aa070f0b13
Muhammad Saim Ali Siddiqui <106109352+saimsiddiqui8@users.noreply.github.com>
2023-04-19 14:27:00 +0500

    basic-2
    basic-2

---
+++ basic-2/app.js
@@ -0,0 +1,40 @@
+// Write a function that takes two numbers 
+// as arguments and returns their product.
+
+// function product(num1, num2) {
+//     return num1 * num2;
+// }
+// console.log(product(5, 2));
+
+
+// Write a function that takes an array of numbers
+//  and returns the average.
+
+// function avg() {
+//     let arr = [1, 2, 3, 4, 5];
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum+=arr[i]; 
+//     }
+//     return sum/arr.length;
+// }
+// console.log(avg())
+
+
+// Write a function that takes an array of numbers and returns
+//  the largest number.
+
+
+// function largest() {
+//     let arr = [1, 2, 3, 7, 8, 5, 6, 7, 2];
+//     let index;
+//     let larger = arr[0];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > larger) {
+//             larger = arr[i];
+//             index = i + 1;
+//         }
+//     } 
+//     console.log(larger + " at value " + index);
+// }
+// largest();
