b2bb06a2c752e6eabd4c5d12e75125a478c99d47
Muhammad Saim Ali Siddiqui <106109352+saimsiddiqui8@users.noreply.github.com>
2023-04-20 11:05:01 +0500

    basic-3
    basic-3

---
+++ basic-3/app.js
@@ -0,0 +1,81 @@
+// ************** TASK NUMBER 1 ***************
+
+// Create a simple calculator!
+
+// PART 1:-
+//        Get two numbers (input using prompt) from the user.
+
+// PART 2:-
+//        Create 4 buttons named '+', '-', 'x', '÷' .
+
+// Part 3:-
+//        If the user click '+' button result would be shown in the  console.
+// Simultaneously for the rest of the buttons
+
+// let num1 = +prompt("Enter 1st number");
+// let num2 = +prompt("Enter 2nd number");
+
+// function add() {
+//     console.log(num1 + num2);
+// }
+// function subtract() {
+//     console.log(num1 - num2);
+// }
+// function multiply() {
+//     console.log(num1 * num2);
+// }
+// function divide() {
+//     console.log(num1 / num2);
+// }
+
+
+// objects 
+
+// let obj1 = {
+//     name: "sehar",
+//     phone: "123",
+// }
+// let student1 = {
+//   name: "Hamza",
+//   rollNo: 420,
+//   teacher: "Nadir",
+//   className: "JavaScript"
+// };
+// console.log(student1.teacher);
+
+
+
+// let decisionMaker = ["yes", "no"];
+// // template literals
+// decisionMaker.forEach((gussa) => {
+//     if (gussa === "yes") {
+//         console.log(`${gussa} baat khatam`);
+//     }else{
+//         console.log(gussa);
+//     }
+// })
+
+
+// let person = {
+//     names: "John",
+//     age: 30,
+//     city: "New York",
+// };
+
+// console.log(person.names);
+
+// Write a function that takes an array of numbers and uses
+//  forEach to print each number to the console.
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.forEach((num) => {
+//     //pro programmer
+//     document.write(num);
+// })
+
+
+let arr = ["baat khatam", "block", "gussa", "ignore", "ego"];
+let names = "seharFatimaDev";
+
+console.log(arr.splice(0 ,3));
+console.log(names.slice(0 ,15));
